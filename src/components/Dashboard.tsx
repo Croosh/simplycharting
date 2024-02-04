af3024b52a5a313ce4cee792b14258998e25ae15
Umar A. <umar.ak318@gmail.com>
2024-02-04 16:06:52 +0500

    Added Theme

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,6 +2,7 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { useState } from "react";
 import Chart from "./Chart";
+import { ModeToggle } from "./mode-toggle";
 export default function Dashoard() {
   const [row, setRow] = useState<object[]>([
     {
@@ -27,7 +28,10 @@ export default function Dashoard() {
   };
 
   return (
-    <div className=" h-screen justify-center items-center flex flex-col gap-6">
+    <div className=" h-screen relative  justify-center items-center flex flex-col gap-6">
+      <div className="absolute top-1  right-5">
+        <ModeToggle />
+      </div>
       <div className="flex gap-2">
         <Input
           placeholder="Label"
